6a2ea16de3b6dfe7b2b4fa98857f371c37772175
Swagata-88 <itzofficial2025@gmail.com>
2026-02-14 14:04:04 +0530

    Final RF model with validation checks and metrics

--- src/models/linear_regression_model.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LinearRegression
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-
-def linear_regression_model():
-    return Pipeline([
-        ("scaler", StandardScaler()),
-        ("model", LinearRegression())
-    ])
